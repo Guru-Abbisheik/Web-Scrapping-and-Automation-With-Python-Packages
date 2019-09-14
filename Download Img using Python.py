c6d9c43281cbba66927a48c072790dd1e5f1eed9
Guru-Abbisheik <53990046+Guru-Abbisheik@users.noreply.github.com>
2019-09-14 11:59:40 +0530

    Download Img using Python Package "Urllib"

---
+++ Download Img using Python.py
@@ -0,0 +1,5 @@
+import urllib.request
+
+url_img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Muralitharan_bowling_to_Adam_Gilchrist.jpg/499px-Muralitharan_bowling_to_Adam_Gilchrist.jpg'
+
+urllib.request.urlretrieve(url_img, 'muthu.jpg')
